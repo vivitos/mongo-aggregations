ea32aa1e9eae13d5b6f5d63e15b07e881f42292f
Vivien BRUNEL <vbrunel@EKI-PC00011.local>
2018-05-22 11:00:37 +0200

    conf: Test with nyx database

--- conf/development.js
+++ conf/development.js
@@ -1,8 +1,8 @@
 module.exports = {
 	mono: {
 		mongodb: {
-			url: 'mongodb://localhost:27017',
-			dbName: 'node-skeleton'
+			url: `mongodb://eki_admin:c3ab8ff13720e8ad9047dd39466b3c8974e592c2fa383d4a3960714caef0c4f2@aws-eu-central-1-portal.2.dblayer.com:15942,aws-eu-central-1-portal.1.dblayer.com:15942/nyx_dev?ssl=true`,
+			dbName: 'nyx_dev'
 		},
 	}
 }
