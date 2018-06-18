a16a0b424e0b1604de618ba870663be4150177da
Vivitos <vivien.brunel21@gmail.com>
2018-06-18 11:17:06 +0200

    conf: Update conf with new database

--- conf/development.js
+++ conf/development.js
@@ -1,8 +1,8 @@
 module.exports = {
 	mono: {
 		mongodb: {
-			url: `mongodb://eki_admin:c3ab8ff13720e8ad9047dd39466b3c8974e592c2fa383d4a3960714caef0c4f2@aws-eu-central-1-portal.2.dblayer.com:15942,aws-eu-central-1-portal.1.dblayer.com:15942/nyx_dev?ssl=true`,
-			dbName: 'nyx_dev'
+			url: 'mongodb://eki_admin:eki136CE@aws-us-east-1-portal.24.dblayer.com:28972,aws-us-east-1-portal.29.dblayer.com:28972/dashboard-gen-dev?ssl=true',
+			dbName: 'dashboard-gen-dev'
 		},
 	}
 }
