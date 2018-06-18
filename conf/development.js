075b3bc2b6f6274c0d7379e5c3b9a61206dccb99
Vivitos <vivien.brunel21@gmail.com>
2018-06-18 13:56:27 +0200

    conf: Remove conf files

--- conf/development.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	mono: {
-		mongodb: {
-			url: 'mongodb://eki_admin:eki136CE@aws-us-east-1-portal.24.dblayer.com:28972,aws-us-east-1-portal.29.dblayer.com:28972/dashboard-gen-dev?ssl=true',
-			dbName: 'dashboard-gen-dev'
-		},
-	}
-}
--- conf/test.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	mono: {
-		mongodb: {
-			url: 'mongodb://localhost:27017',
-			dbName: 'node-skeleton-test',
-			dropDatabase: true
-		},
-	}
-}
